f239a64c1d5340f0f8b06d974e03a65593b93bef
Ria Legaspi <riaveluz@gmail.com>
2023-02-05 21:38:09 -0800

    commit tree 72ac302b4f2c098cce8bc46063c7f1811ea55eb1

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,18 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  def kind 
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
 end
